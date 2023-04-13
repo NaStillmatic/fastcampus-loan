fe30bdc857e0381b5e64360645fd5619be6b6b1a
Joe <nastillmatic@gmail.com>
2023-04-13 18:45:37 +0900

    CH-03 대출 신청 서류 CRUD 기능 구현

--- src/main/java/com/fastcampus/loan/controller/ApplicationController.java
+++ src/main/java/com/fastcampus/loan/controller/ApplicationController.java
@@ -3,10 +3,21 @@ package com.fastcampus.loan.controller;
 import com.fastcampus.loan.dto.ApplicationDTO.AcceptTerms;
 import com.fastcampus.loan.dto.ApplicationDTO.Response;
 import com.fastcampus.loan.dto.ApplicationDTO.Request;
+import com.fastcampus.loan.dto.FileDTO;
 import com.fastcampus.loan.dto.ResponseDTO;
 import com.fastcampus.loan.service.ApplicationService;
+import com.fastcampus.loan.service.FileStorageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @RestController
@@ -14,6 +25,7 @@ import org.springframework.web.bind.annotation.*;
 public class ApplicationController extends AbstractController {
 
     private final ApplicationService applicationService;
+    private final FileStorageService fileStorageService;
 
     @PostMapping
     public ResponseDTO<Response> create(@RequestBody Request request) {
@@ -39,5 +51,36 @@ public class ApplicationController extends AbstractController {
     @PostMapping("/{applicationId}/terms")
     public ResponseDTO<Boolean> acceptTerms(@PathVariable Long applicationId, @RequestBody AcceptTerms request) {
         return ok(applicationService.acceptTerms(applicationId, request));
+    }
+
+    @PostMapping("/{applicationId}/files")
+    public ResponseDTO<Void> upload(@PathVariable Long applicationId, MultipartFile file) throws IllegalStateException, IOException {
+        fileStorageService.save(applicationId, file);
+        return ok();
+    }
+
+    @GetMapping("/{applicationId}/files")
+    public ResponseEntity<Resource> dowonload(@PathVariable Long applicationId, @RequestParam(value = "filename") String filename) throws IllegalStateException, IOException {
+        Resource file = fileStorageService.load(applicationId, filename);
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
+
+    @DeleteMapping("/{applicationId}/files")
+    public ResponseDTO<Void> deleteAll(@PathVariable Long applicationId) {
+        fileStorageService.deleteAll(applicationId);
+        return ok();
+    }
+
+    @GetMapping("/{applicationId}/files/infos")
+    public ResponseDTO<List<FileDTO>> getFileInfos(@PathVariable Long applicationId) {
+        List<FileDTO> fileInfos = fileStorageService.loadAll(applicationId).map(path -> {
+            String filename = path.getFileName().toString();
+            return FileDTO.builder()
+                    .name(filename)
+                    .url(MvcUriComponentsBuilder.fromMethodName(ApplicationController.class, "download", applicationId, filename).build().toString()).build();
+        }).collect(Collectors.toList());
+
+        return ok(fileInfos);
     }
 }
